d3a93918bcfaba637cab9d1600936208073d034e
amitnishu15 <amitnishu15@gmail.com>
2020-06-21 02:15:38 +0530

    Third deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import pimage from './img/pic.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
+      <img src={pimage} alt="p-image" />
     </div>
   );
 }
