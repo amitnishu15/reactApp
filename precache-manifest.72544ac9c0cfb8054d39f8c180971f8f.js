9cb8328670bab0b30d80823a6ec57d4243d18092
amitnishu15 <amitnishu15@gmail.com>
2020-06-20 22:35:38 +0530

    Updates

---
+++ precache-manifest.72544ac9c0cfb8054d39f8c180971f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53037160d48d61f45eebfd40543130a",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "0a52d664fc06ac0c8811",
+    "url": "/reactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/reactApp/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a52d664fc06ac0c8811",
+    "url": "/reactApp/static/js/main.3c1aaaa2.chunk.js"
+  },
+  {
+    "revision": "2294fe26470388394114",
+    "url": "/reactApp/static/js/runtime-main.02d2b9ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
